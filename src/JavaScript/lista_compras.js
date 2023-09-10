52e78d4b76340fc54c0de7db4ddad36594f29dc8
Melvin Rivas <117697077+andres95monsalve@users.noreply.github.com>
2023-09-09 22:28:58 -0400

    Modificacion de Estructura

--- src/JavaScript/lista_compras.js
+++ src/JavaScript/lista_compras.js
@@ -39,7 +39,7 @@ function saveList() {
     const doc = new jsPDF('p', 'mm', 'a4');
 
     const image = new Image();
-    image.src = '/Imagenes/PDF.jpg';
+    image.src = 'src/Imagenes/PDF.jpg';
 
     image.onload = function() {
         const imgWidth = 120;
